0e748e28a59dd99947b833b754af07269b9e15a2
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2023-09-28 09:59:35 +0300

    work

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public String longestCommonPrefix(String[] args) {
      Arrays.sort(args, Comparator.comparing(String::length));
 
         if(args.length == 0) {
