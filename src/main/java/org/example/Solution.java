7f176a50368e822d91260b9c6b742cd74c3cec8b
AliakseiBaryliuk-E <124911182+AliakseiBaryliuk-E@users.noreply.github.com>
2023-09-28 09:36:58 +0300

    is changed for plag

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,38 @@ package org.example;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        //write your code here
+     Arrays.sort(args, Comparator.comparing(String::length));
+
+        if(args.length == 0) {
+
+            return "";
+
+        }
+
+        for (int i = 1; i < args.length; i++) {
+
+            args[i] = args[i].substring(0, args[0].length());
+
+        }
+
+        while (!args[0].isEmpty()) {
+
+            if (Arrays.stream(args).skip(1).allMatch(s -> args[0].equals(s))) {
+
+                return args[0];
+
+            }
+            int length = args[0].length();
+            length--;
+
+            for (int i = 0; i < args.length; i++) {
+
+                args[i] = args[i].substring(0, length);
+
+            }
+
+        }
+
         return "";
     }
 }
